c82f8d4329634a06a797a684a5723a937a2e0f13
Ritika <ritz21ch@gmail.com>
2024-07-24 14:12:12 +0530

    update rontend files

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
-  );
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+        </Routes>
+      </Router>
+    );
+  }
 }
-
-export default HomePage;
